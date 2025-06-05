28aeeaec6b80592c8e04e26b9b583b230bb9134b
xingakapas <alta2104@student.abudzar.sch.id>
2025-06-05 08:37:25 +0700

    kode utama

---
+++ my-project/src/SAS.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './Org.png';
+const SAS = () => {
+  return (
+    <div>
+      <h1>Jawa</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui adipisci quibusdam quasi eum, voluptatibus fugiat maiores repellat maxime expedita repudiandae, aperiam similique minima? Ipsum quo culpa recusandae unde dolorum maxime nulla consequatur perspiciatis numquam ipsa iure natus, totam quos, 
+        similique facilis exercitationem corporis iste autem minus aliquam sapiente laborum repellat?.</p>
+<img src={logo} />
+    </div>
+  );
+};
+
+export default SAS;
